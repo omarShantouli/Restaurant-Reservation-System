7637a4fabea18d88ed7b310dab160dfa3492c8d7
samigogo <samigogo339@gmial.com>
2023-11-02 14:41:22 +0200

    create and seed migrations

--- Resturant Reservations Domain/Customers.cs
+++ Resturant Reservations Domain/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace ResturantDataModel
 {
     public class Customers
     {
+        [Key]
         public int customer_id { get; set; }
         public string first_name { get; set; }
         public string last_name { get; set;}
--- Resturant Reservations Domain/Resturants.cs
+++ Resturant Reservations Domain/Resturants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,11 @@ namespace ResturantDataModel
 {
     public class Resturants
     {
+        [Key]
         public int resturant_id { get; set; }
         public string name { get; set; }
         public string address { get; set; }
         public string phone_number { get; set; }
-        List<TimeOnly> openning_hours { get; set; }
+        public string opening_hours { get; set; }
     }
 }
