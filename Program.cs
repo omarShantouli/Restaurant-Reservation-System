ac7414a1fe4fd5c652eb38e422c6d834af42d958
samigogo <samigogo339@gmial.com>
2023-11-05 19:42:19 +0200

    Creating Utils folder, and adding RestaurantReservationsOperations that includes some methods, and demonstrating the functionality of all the methods by calling them with sample data for testing purposes.

--- Program.cs
+++ Program.cs
@@ -10,18 +10,38 @@ namespace RestaurantReservation
     {
         static void Main(string[] args)
         {
-            var customerRepository = new Repository<Customers>();
-            Customers customer = new Customers
-            {
-                first_name = "Ahmed",
-                last_name = "Turkman",
-                email = "Turkman.doe@example.com",
-                phone_number = "+1-555-555-5555"
-            };
-           // customerRepository.Create(customer);
-            customerRepository.Delete(1002);
+           // var customerRepository = new Repository<Customers>();
+           // var restrepo = new Repository<Resturants>();
+           // Customers customer = new Customers
+           // {
+           //     first_name = "Ahmed",
+           //     last_name = "Turkman",
+           //     email = "Turkman.doe@example.com",
+           //     phone_number = "+1-555-555-5555"
+           // };
+           //// customerRepository.Create(customer);
+           //// customerRepository.Delete(1002);
+           // var rests = restrepo.GetAll();
+           // foreach(var restaurant in rests)
+           // {
+           //     Console.WriteLine(restaurant.address + " " + restaurant.name);
+           // }
+           var restaurantReservationsOperations = new RestaurantReservationsOperations();
+            //var employees = restaurantReservationsOperations.ListManagers();
+            //foreach (var e in employees)
+            //{
+            //    Console.WriteLine(e.first_name + " " + e.last_name);
+            //}
+            //var reservations = restaurantReservationsOperations.GetReservationsByCustomer(103);
+            //foreach(var reservation in reservations)
+            //{
+            //    Console.WriteLine(reservation.reservation_id + " " + reservation.reservation_date);
+            //}
+            // restaurantReservationsOperations.ListOrdersAndMenuItems(1);
+            //Console.WriteLine(restaurantReservationsOperations.CalculateAverageOrderAmount(3)); 
+
         }
 
-      
+
     }
 }
---
+++ Resturant Reservations Db/Resturant Reservations Domain/Utils/RestaurantReservationsOperations.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+using ResturantDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant_Reservations_Db.Resturant_Reservations_Domain
+{
+    public class RestaurantReservationsOperations
+    {
+        private readonly RestaurantReservationDbContext _context;
+        public RestaurantReservationsOperations()
+        {
+            _context= new RestaurantReservationDbContext();
+        }
+
+        public List<Employees> ListManagers()
+        {
+            var employees = _context.Employees.Where(e => e.position == "Manager").ToList();
+            return employees;
+        }
+
+        public List<Reservations> GetReservationsByCustomer(int CustomerId)
+        {
+            var reservations = _context.Reservations.Where(r => r.customer_id== CustomerId).ToList();
+            return reservations;
+        }
+
+        public void ListOrdersAndMenuItems(int ReservationId)
+        {
+            var ordersAndMenuItems = _context.Orders
+                                       .Where(o => o.reservation_id == ReservationId)
+                                       .Join(_context.OrderItems, o => o.order_id, oi => oi.order_id, (o, oi) => new
+                                       {
+                                           order_id = o.order_id,
+                                           OrderItem = oi
+                                       })
+                                       .Join(_context.MenuItems, joined => joined.OrderItem.item_id, mi => mi.item_id, (joined, mi) => new 
+                                       {
+                                           order_id = joined.order_id,
+                                           MenuItems = new List<MenuItems> { mi }
+                                       })
+                                       .ToList();
+
+            foreach (var menuItem in ordersAndMenuItems)
+            {   
+                Console.WriteLine($"Order ID: {menuItem.order_id}");
+
+                Console.WriteLine("Order Items:");
+                foreach (var item in menuItem.MenuItems)
+                {
+                    Console.WriteLine($"  Item ID: {item.item_id}\n" +
+                                      $"  Restaurant ID: {item.resturant_id}\n" +
+                                      $"  Item Name: {item.name}\n" +
+                                      $"  Description: {item.description}\n" +
+                                      $"  Price: {item.price}\n");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public decimal CalculateAverageOrderAmount(int EmployeeId)
+        {
+            var result = _context.Orders.GroupBy(o => o.employee_id)
+                .Select(group => new
+                {
+                    employee_id = group.Key,
+                    avg = group.Average(o => o.total_amount)
+                }).Where(res => res.employee_id == EmployeeId).SingleOrDefault();
+
+             return result.avg;
+        }
+
+    }
+}
