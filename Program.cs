223c79eeff4932fe91e13ec79726e02508c93442
samigogo <samigogo339@gmial.com>
2023-11-05 12:42:27 +0200

    creating a generic repository interface and a generic repository class to implement the Create, Update, and Delete operations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using RestaurantReservation.Db;
+using Resturant_Reservations_Db.Resturant_Reservations_Domain;
 using ResturantDataModel;
 
 namespace RestaurantReservation
@@ -9,7 +10,16 @@ namespace RestaurantReservation
     {
         static void Main(string[] args)
         {
-
+            var customerRepository = new Repository<Customers>();
+            Customers customer = new Customers
+            {
+                first_name = "Ahmed",
+                last_name = "Turkman",
+                email = "Turkman.doe@example.com",
+                phone_number = "+1-555-555-5555"
+            };
+           // customerRepository.Create(customer);
+            customerRepository.Delete(1002);
         }
 
       
---
+++ Resturant Reservations Db/Resturant Reservations Domain/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant_Reservations_Db.Resturant_Reservations_Domain
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly RestaurantReservationDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public Repository() 
+        {
+            _context = new RestaurantReservationDbContext();
+            _dbSet= _context.Set<TEntity>();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+        public void Create(TEntity entity) 
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity) 
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+        }
+        public void Delete(int id) 
+        {
+            TEntity entityToDelete = GetById(id);
+            if (entityToDelete != null)
+            {
+                _dbSet.Remove(entityToDelete);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
