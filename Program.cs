e52c0039bcecc39cc1734549dbcb26a38e469486
samigogo <samigogo339@gmial.com>
2023-11-06 11:43:25 +0200

    Developing FindCustomersWithLargeParties method in RestaurantReservation.Db project and calling it in another FindCustomersWithLargeParties method in RestaurantReservationsOperations to execute the stored procedure.

--- Program.cs
+++ Program.cs
@@ -10,44 +10,57 @@ namespace RestaurantReservation
     {
         static void Main(string[] args)
         {
-            //var customerRepository = new Repository<Customers>();
-            //var restrepo = new Repository<Resturants>();
+            var customerRepository = new Repository<Customers>();
+            var restrepo = new Repository<Resturants>();
 
-            //Customers customer = new Customers
-            //{
-            //    first_name = "Ahmed",
-            //    last_name = "Turkman",
-            //    email = "Turkman.doe@example.com",
-            //    phone_number = "+1-555-555-5555"
-            //};
-            //customerRepository.Create(customer);
+            Customers customer = new Customers
+            {
+                first_name = "Ahmed",
+                last_name = "Turkman",
+                email = "Turkman.doe@example.com",
+                phone_number = "+1-555-555-5555"
+            };
+            customerRepository.Create(customer);
 
-            //customerRepository.Delete(customer.customer_id);
+            customerRepository.Delete(customer.customer_id);
 
-            //var rests = restrepo.GetAll();
-            //foreach (var restaurant in rests)
-            //{
-            //    Console.WriteLine(restaurant.address + " " + restaurant.name);
-            //}
+            var rests = restrepo.GetAll();
+            foreach (var restaurant in rests)
+            {
+                Console.WriteLine(restaurant.address + " " + restaurant.name);
+            }
 
             var restaurantReservationsOperations = new RestaurantReservationsOperations();
 
-            //var employees = restaurantReservationsOperations.ListManagers();
-            //foreach (var e in employees)
-            //{
-            //    Console.WriteLine(e.first_name + " " + e.last_name);
-            //}
+            var employees = restaurantReservationsOperations.ListManagers();
+            foreach (var e in employees)
+            {
+                Console.WriteLine(e.first_name + " " + e.last_name);
+            }
 
-            //var reservations = restaurantReservationsOperations.GetReservationsByCustomer(103);
-            //foreach (var reservation in reservations)
-            //{
-            //    Console.WriteLine(reservation.reservation_id + " " + reservation.reservation_date);
-            //}
+            var reservations = restaurantReservationsOperations.GetReservationsByCustomer(103);
+            foreach (var reservation in reservations)
+            {
+                Console.WriteLine(reservation.reservation_id + " " + reservation.reservation_date);
+            }
 
-            //restaurantReservationsOperations.ListOrdersAndMenuItems(1);
-            //Console.WriteLine(restaurantReservationsOperations.CalculateAverageOrderAmount(3));
+            restaurantReservationsOperations.ListOrdersAndMenuItems(1);
+            Console.WriteLine(restaurantReservationsOperations.CalculateAverageOrderAmount(3));
 
             Console.WriteLine(restaurantReservationsOperations.GetTotalRevenueForRestaurant(1));
+
+            var customersWithLargeParties = restaurantReservationsOperations.FindCustomersWithLargeParties(4);
+            foreach(var Customer in customersWithLargeParties)
+            {
+               
+                Console.WriteLine($"Customer ID: {Customer.customer_id},\n" +
+                     $"  first name: {Customer.first_name},\n" +
+                     $"  last name: {Customer.last_name},\n" +
+                     $"  email: {Customer.email}," +
+                     $"  phone number: {Customer.phone_number}\n");
+                Console.WriteLine();
+
+            }
 
         }
 
---
+++ Resturant Reservations Db/Migrations/20231106091455_CreateFindCustomersWithLargePartiesProcedure.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Resturant_Reservations_Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateFindCustomersWithLargePartiesProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                                    CREATE PROCEDURE FindCustomersWithLargeParties
+                                        @PartySizeThreshold INT
+                                    AS
+                                    BEGIN
+                                        SELECT c.*
+                                        FROM Customers c
+                                        INNER JOIN Reservations r ON c.customer_id = r.customer_id
+                                        WHERE r.party_size > @PartySizeThreshold;
+                                    END
+                                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS FindCustomersWithLargeParties;");
+        }
+    }
+}
