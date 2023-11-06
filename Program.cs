5380761f3a50aa0244aba46de02a5b2d8d0bbd13
samigogo <samigogo339@gmial.com>
2023-11-06 10:17:37 +0200

    creating addView2 migration that creates the EmployeeRestaurantView

--- Program.cs
+++ Program.cs
@@ -10,42 +10,44 @@ namespace RestaurantReservation
     {
         static void Main(string[] args)
         {
-            var customerRepository = new Repository<Customers>();
-            var restrepo = new Repository<Resturants>();
+            //var customerRepository = new Repository<Customers>();
+            //var restrepo = new Repository<Resturants>();
 
-            Customers customer = new Customers
-            {
-                first_name = "Ahmed",
-                last_name = "Turkman",
-                email = "Turkman.doe@example.com",
-                phone_number = "+1-555-555-5555"
-            };
-            customerRepository.Create(customer);
+            //Customers customer = new Customers
+            //{
+            //    first_name = "Ahmed",
+            //    last_name = "Turkman",
+            //    email = "Turkman.doe@example.com",
+            //    phone_number = "+1-555-555-5555"
+            //};
+            //customerRepository.Create(customer);
 
-            customerRepository.Delete(customer.customer_id);
+            //customerRepository.Delete(customer.customer_id);
 
-            var rests = restrepo.GetAll();
-            foreach (var restaurant in rests)
-            {
-                Console.WriteLine(restaurant.address + " " + restaurant.name);
-            }
+            //var rests = restrepo.GetAll();
+            //foreach (var restaurant in rests)
+            //{
+            //    Console.WriteLine(restaurant.address + " " + restaurant.name);
+            //}
 
-            var restaurantReservationsOperations = new RestaurantReservationsOperations();
+            //var restaurantReservationsOperations = new RestaurantReservationsOperations();
 
-            var employees = restaurantReservationsOperations.ListManagers();
-            foreach (var e in employees)
-            {
-                Console.WriteLine(e.first_name + " " + e.last_name);
-            }
+            //var employees = restaurantReservationsOperations.ListManagers();
+            //foreach (var e in employees)
+            //{
+            //    Console.WriteLine(e.first_name + " " + e.last_name);
+            //}
 
-            var reservations = restaurantReservationsOperations.GetReservationsByCustomer(103);
-            foreach (var reservation in reservations)
-            {
-                Console.WriteLine(reservation.reservation_id + " " + reservation.reservation_date);
-            }
+            //var reservations = restaurantReservationsOperations.GetReservationsByCustomer(103);
+            //foreach (var reservation in reservations)
+            //{
+            //    Console.WriteLine(reservation.reservation_id + " " + reservation.reservation_date);
+            //}
 
-            restaurantReservationsOperations.ListOrdersAndMenuItems(1);
-            Console.WriteLine(restaurantReservationsOperations.CalculateAverageOrderAmount(3));
+            //restaurantReservationsOperations.ListOrdersAndMenuItems(1);
+            //Console.WriteLine(restaurantReservationsOperations.CalculateAverageOrderAmount(3));
+
+
 
         }
 
